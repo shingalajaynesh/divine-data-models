16a0a340ff9baf987273e31ae9c0e17755b62f81
shingalajaynesh <shingala.jaynesh@gmail.com>
2026-07-06 16:31:52 +0530

    feat: add Inquiry and InquiryResponse data models and integrate them into DataModels class

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ export * from "./lib/center.js";
 export * from "./lib/role.js";
 export * from "./lib/user.js";
 export * from "./lib/crypto.js";
+export { default as inquiry } from "./lib/inquiry.js";
+export { default as inquiryResponse } from "./lib/inquiryResponse.js";
 export { DataModels } from "./lib/data-models-class.js";
